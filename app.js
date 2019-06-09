3eb8e2ffff613d529510e7fcb5e53f97e7fd7208
Maximilian Riley <maximilianriley@Maximilians-MacBook-Pro.local>
2019-06-09 23:16:47 +0100

    livingOnAPrayer

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
 // Play drum by keyboard press
+// let boomm = new Audio("../sounds");
+// boom.src = "boom.wav"
+
+const q = document.getElementById('q');
+const w = document.getElementById('w');
+const e = document.getElementById('e');
+const r = document.getElementById('r');
+const t = document.getElementById('t');
+const y = document.getElementById('y');
+const u = document.getElementById('u');
+const i = document.getElementById('i');
+const o = document.getElementById('o');
+
+q.addEventListener('click', boom() => {
+    document.getElementById('boom');
+    let boom = new Audio(../sounds/: "boom.wav")
+})
+q.onclick = function () {
+    boom();
+}
+
+
 
 // Play music by mouse click
