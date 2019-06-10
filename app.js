51fcbcdf0ca39e56e71353ee057360ef2c4a7566
CN0107 <cn0107@CN0107.lan>
2019-06-10 15:32:19 +0100

    soundOnClickDone

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-// Play drum by keyboard press
-// let boomm = new Audio("../sounds");
-// boom.src = "boom.wav"
-
+const boom = new Audio('./sounds/boom.wav');
+const clap = new Audio('./sounds/clap.wav');
+const hihat = new Audio('./sounds/hihat.wav');
+const kick = new Audio('./sounds/kick.wav');
+const openhat = new Audio('./sounds/openhat.wav');
+const ride = new Audio('./sounds/ride.wav');
+const snare = new Audio('./sounds/snare.wav');
+const tink = new Audio('./sounds/tink.wav');
+const tom = new Audio('./sounds/tom.wav');
 const q = document.getElementById('q');
 const w = document.getElementById('w');
 const e = document.getElementById('e');
@@ -12,14 +17,37 @@ const u = document.getElementById('u');
 const i = document.getElementById('i');
 const o = document.getElementById('o');
 
-q.addEventListener('click', boom() => {
-    document.getElementById('boom');
-    let boom = new Audio(../sounds/: "boom.wav")
-})
-q.onclick = function () {
-    boom();
-}
+// Play drum by keyboard press
 
 
 
 // Play music by mouse click
+
+q.addEventListener('click', () => {
+    console.log('clicked')
+    boom.play();
+})
+w.addEventListener('click', () => {
+    clap.play();
+})
+e.addEventListener('click', () => {
+    hihat.play();
+})
+r.addEventListener('click', () => {
+    kick.play();
+})
+t.addEventListener('click', () => {
+    openhat.play();
+})
+y.addEventListener('click', () => {
+    ride.play();
+})
+u.addEventListener('click', () => {
+    snare.play();
+})
+i.addEventListener('click', () => {
+    tink.play();
+})
+o.addEventListener('click', () => {
+    tom.play();
+})
